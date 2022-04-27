f8fd7927905988cda44d88389fa6150c4109ce3e
christopher <cdnance1829@gmail.com>
2022-04-27 00:54:19 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click', function(){
+        alert('I was clicked!');
+    });
 }
+
+/*function bangAlert() {
+    alert('I too, role with a "click"');
+}*/
+
+//const thirdAlert = () => { alert('I\' m with the click too') }
